996f9ee6ee8476887fa05a4a5144342d48a56677
bcooper085 <bcooper085@gmail.com>
2017-02-24 11:40:07 -0800

    add client object with getall, save, update and delete methods, tests pass

---
+++ Objects/Client.cs
@@ -0,0 +1,162 @@
+using System.Data.SqlClient;
+using System.Data;
+using System;
+using System.Collections.Generic;
+
+namespace BarksApp
+{
+    public class Client
+    {
+        private string _name;
+        private int _id;
+
+        public Client(string name, int Id = 0)
+        {
+            _name = name;
+            _id = Id;
+        }
+        public string GetName()
+        {
+            return _name;
+        }
+        public int GetId()
+        {
+            return _id;
+        }
+
+        public static void DeleteAll()
+        {
+            SqlConnection conn = DB.Connection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("DELETE FROM clients;", conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        public static List<Client> GetAll()
+        {
+            List<Client> allClients = new List<Client>();
+            SqlConnection conn = DB.Connection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM clients;", conn);
+
+            SqlDataReader rdr = cmd.ExecuteReader();
+
+            while(rdr.Read())
+            {
+                int foundId = rdr.GetInt32(0);
+                string foundName = rdr.GetString(1);
+                Client foundClient = new Client(foundName, foundId);
+                allClients.Add(foundClient);
+            }
+            if(rdr != null)
+            {
+                rdr.Close();
+            }
+            if(conn != null)
+            {
+                rdr.Close();
+            }
+
+            return allClients;
+        }
+
+        public void Save()
+        {
+            SqlConnection conn = DB.Connection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("INSERT INTO clients(name) OUTPUT INSERTED.id VALUES(@ClientName)", conn);
+
+            SqlParameter nameParameter = new SqlParameter();
+            nameParameter.ParameterName = "@ClientName";
+            nameParameter.Value = this.GetName();
+            cmd.Parameters.Add(nameParameter);
+
+            SqlDataReader rdr = cmd.ExecuteReader();
+
+            while(rdr.Read())
+            {
+                this._id = rdr.GetInt32(0);
+            }
+
+            if(rdr != null)
+            {
+                rdr.Close();
+            }
+
+            if(conn != null)
+            {
+                conn.Close();
+            }
+        }
+        public void UpdateName(string newName)
+        {
+            SqlConnection conn = DB.Connection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("UPDATE clients SET name = @NewName OUTPUT INSERTED.name WHERE id = @ClientId;", conn);
+            SqlParameter newNameParameter = new SqlParameter();
+            newNameParameter.ParameterName = "@NewName";
+            newNameParameter.Value = newName;
+            cmd.Parameters.Add(newNameParameter);
+
+            SqlParameter idParameter = new SqlParameter();
+            idParameter.ParameterName = "@ClientId";
+            idParameter.Value = this.GetId();
+            cmd.Parameters.Add(idParameter);
+
+            SqlDataReader rdr = cmd.ExecuteReader();
+
+            while(rdr.Read())
+            {
+                this._name = rdr.GetString(0);
+            }
+
+            if(rdr != null)
+            {
+                rdr.Close();
+            }
+            if(conn != null)
+            {
+                conn.Close();
+            }
+        }
+        public static void DeleteClient(int id)
+        {
+            SqlConnection conn = DB.Connection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("DELETE FROM clients WHERE id = @ClientId;", conn);
+
+            SqlParameter idParameter = new SqlParameter();
+            idParameter.ParameterName = "@ClientId";
+            idParameter.Value = id;
+            cmd.Parameters.Add(idParameter);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+
+
+
+
+//Override
+        public override bool Equals(System.Object otherClient)
+        {
+            if(!(otherClient is Client))
+            {
+                return false;
+            }
+            else
+            {
+                Client newClient = (Client) otherClient;
+                bool idEquality = (this.GetId() == newClient.GetId());
+                bool nameEquality = (this.GetName() == newClient.GetName());
+                return(idEquality && nameEquality);
+            }
+        }
+    }
+}
---
+++ Tests/ClientTest.cs
@@ -0,0 +1,73 @@
+using System.Data.SqlClient;
+using System.Data;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace BarksApp
+{
+    public class ClientTest : IDisposable
+    {
+        public ClientTest()
+        {
+            DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=hair_salon_test;Integrated Security=SSPI;";
+        }
+        public void Dispose()
+        {
+            Client.DeleteAll();
+        }
+
+        [Fact]
+        public void GetAll_IfEmptyOnLoad_Empty()
+        {
+            int result = Client.GetAll().Count;
+
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void Test_IfNameIsEqual_Equal()
+        {
+            Client one = new Client("Fran", 1);
+            Client two = new Client("Fran", 1);
+
+            Assert.Equal(one, two);
+        }
+
+        [Fact]
+        public void Save_SaveClientToDatabase_Save()
+        {
+            Client newClient = new Client("Fran", 1);
+            newClient.Save();
+
+            List<Client> result = Client.GetAll();
+            List<Client> testResult = new List<Client>{newClient};
+
+            Assert.Equal(testResult, result);
+        }
+
+        [Fact]
+        public void UpdateName_EditClientInDatabase_Edit()
+        {
+            Client newClient = new Client("Fran", 1);
+            newClient.Save();
+
+            newClient.UpdateName("Francisco");
+
+            Assert.Equal("Francisco", newClient.GetName());
+        }
+
+        [Fact]
+        public void Delete_DeleteClientFromDatabase_Delete()
+        {
+            Client newClient = new Client("Fran", 1);
+            newClient.Save();
+            Client.DeleteClient(newClient.GetId());
+
+            List<Client> testResult = new List<Client>{};
+            List<Client> result = Client.GetAll();
+
+            Assert.Equal(testResult, result);
+        }
+    }
+}
